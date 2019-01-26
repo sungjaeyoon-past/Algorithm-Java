da3e7c04f741355b97f030f9a5d6b18656cf13fb
sungjae <sungjaeyoon@218.38.137.28>
2019-01-27 00:12:58 +0900

    프로그래머스 프린터 문

--- src/bacjoon/P1463.java
+++ src/bacjoon/P1463.java
@@ -1,10 +1,29 @@
 package bacjoon;
 
+import java.util.Scanner;
+
 public class P1463 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		
+		int[] DP=new int[a+1];
+		DP[0]=0; DP[1]=0;	
+		
+		for(int i=2;i<=a;i++) {
+			DP[i]=DP[i-1]+1;
+			if(i%2==0) {
+				DP[i]=Math.min(DP[i], DP[i/2]+1);
+			}
+			if(i%3==0) {
+				DP[i]=Math.min(DP[i], DP[i/3]+1);				
+			}
+		}
+		System.out.println(DP[a]);
+		
+		
 	}
 
 }
