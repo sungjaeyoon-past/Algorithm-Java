def47055619f6197754e6eb283b674e83af83663
codingHack <dbstjdwo1000@naver.com>
2019-01-31 16:28:20 +0900

    DFS 자

--- src/Programmers/StockPrice.java
+++ src/Programmers/StockPrice.java
@@ -2,9 +2,8 @@ package Programmers;
 
 public class StockPrice {
 	static public int[] solution(int[] prices) {
-//		정확성은 맞지만 효율성에서망한 케이스.
         int[] answer = new int[prices.length];
-        for(int i=0;i<prices.length-1;i++) {//전체를 반
+        for(int i=0;i<prices.length-1;i++) {
         	for(int j=i+1;j<prices.length;j++) {
         		if(prices[i]>prices[j]||j==prices.length-1) { 			
         			answer[i]=j-i;
@@ -14,9 +13,9 @@ public class StockPrice {
         }
         answer[prices.length-1]=0;
         
-        for(int a:answer) {
-        	System.out.print(a+" ");
-        }
+//        for(int a:answer) {
+//        	System.out.print(a+" ");
+//        }
         
         return answer;
 		
