e08a390a8d0af9d06ee29fc2d5fca348017fdb30
codingHack <dbstjdwo1000@naver.com>
2019-02-09 23:32:15 +0900

    skill

--- src/Programmers/MatrixMultiple.java
+++ src/Programmers/MatrixMultiple.java
@@ -1,41 +1,34 @@
 package Programmers;
 
 public class MatrixMultiple {
-	
-	static public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr2[0].length];
-        
-        // 0 [   ]  [   ]
-        // 1 [   ]  [   ]
-        // 2 [   ]  [   ]
-        
-        //0 0, 0 1, 1 0, 1 1, 2 1, 2 2
-        
-        
-        for(int j=0;j<arr1.length;j++) {
-        	for(int i=0;i<arr2[0].length;i++) {
-        		int sum=0;
-        		for(int k=0;k<arr1[0].length;k++) {
-        			sum+=(arr1[j][k]*arr2[k][i]);
-        		}
-        		answer[j][i]=sum;
-        	}
-        }
-        
-        for(int []a:answer) {
-        	for(int b:a) {
-        		System.out.print(b+" ");
-        	}
-        	System.out.println();
-        }
-        
 
-        return answer;
-    }
+	static public int[][] solution(int[][] arr1, int[][] arr2) {
+		int[][] answer = new int[arr1.length][arr2[0].length];
+
+		for (int j = 0; j < arr1.length; j++) {
+			for (int i = 0; i < arr2[0].length; i++) {
+				int sum = 0;
+				for (int k = 0; k < arr1[0].length; k++) {
+					sum += (arr1[j][k] * arr2[k][i]);
+				}
+				answer[j][i] = sum;
+			}
+		}
+
+//        for(int []a:answer) {
+//        	for(int b:a) {
+//        		System.out.print(b+" ");
+//        	}
+//        	System.out.println();
+//        }
+
+		return answer;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [][]arr1= {{1,4},{3,2},{4,1}};
-		int [][]arr2= {{3,3},{3,3}};
+		int[][] arr1 = { { 1, 4 }, { 3, 2 }, { 4, 1 } };
+		int[][] arr2 = { { 3, 3 }, { 3, 3 } };
 		solution(arr1, arr2);
 	}
 
--- src/Programmers/SkillTree.java
+++ src/Programmers/SkillTree.java
@@ -10,7 +10,7 @@ public class SkillTree {
 		}
 
 		for (int j = 0; j < skill_trees.length; j++) {
-			boolean check = false;
+			boolean check = true;
 			for (int i = 0; i < skill.length(); i++) {// 0으로 초기화.
 				skillArr[1][i] = "0";
 			}
@@ -19,40 +19,41 @@ public class SkillTree {
 				tree[i] = skill_trees[j].substring(i, i + 1);
 			}
 
-			for (int i = 0; i < tree.length; i++) { // 돌면서 판
-				for (int k = 0; k < skill.length(); k++) {
-					if (tree[i].equals(skillArr[0][k])) {// 돌면서 판
-						for (int g = 0; g < k; g++) {
-							if (skillArr[1][g] != "1") {
-								check = false;
+			for (int i = 0; i < tree.length; i++) {
+				for (int k = 0; k < skillArr.length; k++) {
+					if (tree[i].equals(skillArr[0][k])) {
+						System.out.println(tree[i]+"와"+skillArr[0][k]);
+						for(int t=0;j<k;j++) {
+							if(skillArr[1][t].equals("0")) {
+								check=false;
 								break;
 							}
-							check=true;
 						}
-						if(check) {							
-							skillArr[1][k] = "1";
+						//맞으면k를 1로함
+						if(check) {
+							skillArr[1][k]="1";
 						}
+						//아니면 false 하브레이
 					}
-					// 아니면 그냥 넘어감.
 				}
 			}
+		
 			if (check) {
 				answer++;
 			}
 
-//			// 출력.
-//			for (String b : tree) {
-//				System.out.print(b + " ");
-//			}
-//			System.out.println();
-
-			// 출력.
-			for (String[] a : skillArr) {
-				for (String b : a) {
-					System.out.print(b + " ");
-				}
-				System.out.println();
+			for (String b : tree) {
+				System.out.print(b + " ");
 			}
+			System.out.println();
+//
+//			// 출력.
+//			for (String[] a : skillArr) {
+//				for (String b : a) {
+//					System.out.print(b + " ");
+//				}
+//				System.out.println();
+//			}
 
 		}
 
@@ -62,7 +63,7 @@ public class SkillTree {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String skill = "CBD";
-		String[] skill_trees = { "BACDE", "CBADF", "AECB", "BDA","ABCDE","CBD" };
+		String[] skill_trees = { "BACDE", "CBADF", "AECB", "BDA", "ABCDE", "CBD" };
 		System.out.println(solution(skill, skill_trees));
 	}
 
