6b435e0bcb335775b4a1cdc52441e78e595e5483
codingHack <dbstjdwo1000@naver.com>
2019-02-13 17:47:05 +0900

    SCV

---
+++ src/ProgrammersHeap/더맵게.java
@@ -0,0 +1,34 @@
+package ProgrammersHeap;
+
+import java.util.*;
+
+public class 더맵게 {
+	static int insertIndex(int num, ArrayList<Integer> arr) {
+		int index=0;
+		Iterator<Integer> it =arr.iterator();
+		
+		
+		return index;
+	}
+
+	static public int solution(int[] scoville, int K) {
+		int answer = 0;
+		ArrayList<Integer> arr = new ArrayList<>();
+		for (int addNum : scoville) {
+			if (arr.isEmpty()) {
+				arr.add(addNum);
+			} else {// 적절한 곳에 추가.
+
+			}
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 1, 2, 3, 9, 10, 12, 5 };
+		solution(arr, 7);
+	}
+
+}
---
+++ src/oncoder/SCV자원캐기.java
@@ -0,0 +1,49 @@
+package oncoder;
+
+public class SCV자원캐기 {
+	static public int solution(String[] map) {
+
+		String[][] arr = new String[map.length][map[0].length() / 2 + 1];
+		int DP[][] = new int[map.length][map[0].length() / 2 + 1];
+		int i = 0;
+		for (String m : map) {
+			arr[i++] = m.split(" ");
+		}
+		for (i = 0; i < DP.length; i++) {
+			for (int j = 0; j < DP[0].length; j++) {
+				if (i == 0 && j == 0) {
+					DP[i][j] = Integer.parseInt(arr[i][j]);
+				} else if (i == 0 && j != 0) {
+					DP[i][j] = DP[i][j - 1]+ Integer.parseInt(arr[i][j]);
+				} else if (i != 0 && j == 0) {
+					DP[i][j] = DP[i - 1][j]+ Integer.parseInt(arr[i][j]);
+				} else {
+					DP[i][j] = Math.max(DP[i][j - 1], DP[i - 1][j]) + Integer.parseInt(arr[i][j]);
+				}
+			}
+		}
+//
+//		for (String[] a : arr) {
+//			for (String b : a) {
+//				System.out.print(b + " ");
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+//		for (int[] a : DP) {
+//			for (int b : a) {
+//				System.out.print(b + " ");
+//			}
+//			System.out.println();
+//		}
+
+		return DP[DP.length-1][DP[0].length-1];
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] map = { "1 0 1", "2 3 1", "2 0 0" };
+		System.out.println(solution(map));
+	}
+
+}
